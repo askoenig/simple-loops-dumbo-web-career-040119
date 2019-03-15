5200f7ca3cb3fa807262701b1b86c5990be6ce87
Andrew Koenig-Bautista <askoenig94@gmail.com>
2019-03-15 01:42:03 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -39,9 +39,7 @@ def until_iterator(number_of_times)
 end
 
 def for_iterator(number_of_times)
-  for number_of_times 1..7
-  
-  
+  for number_of_times in 1..7 do
   puts "Welcome to Flatiron School's Web Development Course!"
   
 end
