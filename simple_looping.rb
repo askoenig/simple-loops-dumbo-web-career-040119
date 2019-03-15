3e31d4f564147625e93216272a9d488a9287ba1e
Andrew Koenig-Bautista <askoenig94@gmail.com>
2019-03-15 01:30:47 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -13,8 +13,8 @@ end
 
 def times_iterator(number_of_times)
   7.times do
-  puts "Welcome to Flatiron School's Web Development Course!"
-end
+    puts "Welcome to Flatiron School's Web Development Course!"
+  end
 end
 
 def while_iterator(number_of_times)
